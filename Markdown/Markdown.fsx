3ee901f00b062a89b6784f6245ebd47efd3b5ee9
htoma <horia.toma@availpro.com>
2016-02-26 12:48:40 +0100

    Working with active patterns

--- Markdown/Markdown.fsx
+++ Markdown/Markdown.fsx
@@ -31,6 +31,27 @@ let parseInline = function
 let toString chars = 
     String(chars |> Array.ofList)
 
+let (|StartsWith|_|) prefix input = 
+    let rec loop = function
+        | p::prefix, r::rest when p=r ->
+            loop(prefix, rest)
+        | [], rest ->
+            Some(rest)
+        | _ -> None
+    loop(prefix, input)
+
+let rec parseBracketedBody closing acc = function
+    | StartsWith closing (rest) ->
+        Some(List.rev acc, rest)
+    | c::chars ->
+        parseBracketedBody closing (c::acc) chars
+    | _ -> None
+
+let parseBracketed opening closing = function
+    | StartsWith opening chars ->
+        parseBracketedBody closing [] chars
+    | _ -> None
+
 let rec parseSpans acc chars = seq {
     let emitLiteral = seq {
         if acc <> [] then
@@ -47,6 +68,8 @@ let rec parseSpans acc chars = seq {
     | _, [] ->
         yield! emitLiteral
 }
+
+let (|Delimited|_|) delim = parseBracketed delim delim
 
 parseSpans [] ("what's this `code` and" |> List.ofSeq)
 |> List.ofSeq
